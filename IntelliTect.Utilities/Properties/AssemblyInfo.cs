678b74511f0aa4b859e526fb729e0cff29d3ab63
Kevin Bost <kitokeboo@gmail.com>
2017-03-01 16:41:22 -0800

    Adding missing description and author for the nuget

--- IntelliTect.Utilities/Properties/AssemblyInfo.cs
+++ IntelliTect.Utilities/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IntelliTect.Utilities")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A utility library for IntelliTect")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("IntelliTect")]
 [assembly: AssemblyProduct("IntelliTect.Utilities")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
