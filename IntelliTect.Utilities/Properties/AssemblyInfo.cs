5eaa759b125e777a7bb8a49cbaa91dd92df8f567
Kelly Adams <kelly@intellitect.com>
2017-05-01 14:07:48 -0700

    Add rules from Mark, enable CodeAnalysis, add dictionary with IntelliTect name in it.

--- IntelliTect.Utilities/Properties/AssemblyInfo.cs
+++ IntelliTect.Utilities/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,9 +11,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("IntelliTect")]
 [assembly: AssemblyProduct("IntelliTect.Utilities")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © IntelliTect 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
