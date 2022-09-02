07e7413151c30e14209fa44074f35bad1e39df45
Benjamin Michaelis <benjamin@michaelis.net>
2022-09-02 14:04:59 -0700

    Make slightly more generic of a test

--- IntelliTect.Multitool.Tests/IPaths.Tests.cs
+++ IntelliTect.Multitool.Tests/IPaths.Tests.cs
@@ -12,7 +12,7 @@ namespace IntelliTect.Multitool.Tests
         {
             // Makes the assumption that the repository directory for this solution is named the same as the solution
             string RepoRoot = $"{nameof(IntelliTect)}.{nameof(Multitool)}.{nameof(Tests)}";
-            Assert.EndsWith($"{nameof(IntelliTect)}.{nameof(Multitool)}", IPaths.GetDefaultRepoRoot($"{nameof(IntelliTect)}.{nameof(Multitool)}.{nameof(Tests)}"));
+            Assert.EndsWith(nameof(Multitool), IPaths.GetDefaultRepoRoot($"{nameof(IntelliTect)}.{nameof(Multitool)}.{nameof(Tests)}"));
         }
     }
 }
