8bfb18edfa7c931b68169bc9c67d44e824669128
Benjamin Michaelis <benjamin@michaelis.net>
2022-09-02 13:52:06 -0700

    Add in IPaths

---
+++ IntelliTect.Multitool.Tests/IPaths.Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Claims;
+using System.Security.Principal;
+using Xunit;
+
+namespace IntelliTect.Multitool.Tests
+{
+    public class IPathsTests
+    {
+        [Fact]
+        public void GetDefaultRepoRoot_ReturnsRepoRootDirectory()
+        {
+            // Makes the assumption that the repository directory for this solution is named the same as the solution
+            string RepoRoot = $"{nameof(IntelliTect)}.{nameof(Multitool)}.{nameof(Tests)}";
+            Assert.EndsWith($"{nameof(IntelliTect)}.{nameof(Multitool)}", IPaths.GetDefaultRepoRoot($"{nameof(IntelliTect)}.{nameof(Multitool)}.{nameof(Tests)}"));
+        }
+    }
+}
---
+++ IntelliTect.Multitool/IPaths.cs
@@ -0,0 +1,32 @@
+﻿namespace IntelliTect.Multitool;
+/// <summary>
+/// Provides normalized paths.
+/// </summary>
+public interface IPaths
+{
+    /// <summary>
+    /// Returns the repository root directory name.
+    /// </summary>
+    /// <param name="CurrentProjectFolderName">The highest level directory name of the current project within the repository.</param>
+    /// <returns></returns>
+    public static string GetDefaultRepoRoot(string CurrentProjectFolderName)
+    {
+        DirectoryInfo? currentDirectory = new(Directory.GetCurrentDirectory());
+
+        while (currentDirectory is not null)
+        {
+            if (currentDirectory.FullName.ToLowerInvariant().EndsWith(CurrentProjectFolderName.ToLowerInvariant(), StringComparison.InvariantCulture))
+            {
+                DirectoryInfo? parent = currentDirectory.Parent;
+                if (parent is not null)
+                {
+                    return parent.FullName;
+                }
+            }
+
+            currentDirectory = currentDirectory.Parent;
+        }
+
+        throw new InvalidOperationException("Could not find the repo root directory from the current directory. Current directory is expected to be the repoRoot sub directory.");
+    }
+}
