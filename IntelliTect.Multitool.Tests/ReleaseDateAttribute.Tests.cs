0cb8c5edd00902ba38378ddc967917a4b57bef75
Benjamin Michaelis <benjamin@michaelis.net>
2023-01-26 10:35:02 -0800

    WIP

---
+++ IntelliTect.Multitool.Tests/ReleaseDateAttribute.Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace IntelliTect.Multitool.Tests;
+
+public class ReleaseDateAttributeTests
+{
+    [Fact]
+    public void ReleaseDateIsGettingInjected()
+    {
+        Assert.NotNull(ReleaseDateAttribute.GetReleaseDate());
+    }
+    [Fact]
+    public void ReleaseDateIsRelativelyCorrectWithinAnHour()
+    {
+        DateTime now = DateTime.Now;
+        Assert.True(ReleaseDateAttribute.GetReleaseDate() > now.AddHours(-1) && ReleaseDateAttribute.GetReleaseDate() <= now.AddHours(1), ReleaseDateAttribute.GetReleaseDate().ToString());
+    }
+}
---
+++ IntelliTect.Multitool/ReleaseDateAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.Reflection;
+
+namespace IntelliTect.Multitool;
+
+/// <summary>
+/// Information about the executing assembly.
+/// </summary>
+[AttributeUsage(AttributeTargets.Assembly)]
+public class ReleaseDateAttribute : Attribute
+{
+    /// <summary> 
+    /// Constructor called from csproj file
+    /// </summary>
+    /// <param name="date"></param>
+    public ReleaseDateAttribute(string date)
+    {
+        ReleaseDate = DateTime.ParseExact(date, "O", CultureInfo.InvariantCulture);
+    }
+    /// <summary>
+    /// The date the assembly was built.
+    /// </summary>
+    public DateTime ReleaseDate { get; }
+    /// <summary>
+    /// Method to obtain the release date from the assembly attributes
+    /// </summary>
+    /// <param name="assembly">An assembly instance</param>
+    /// <returns>A nullable DateTime</returns>
+    public static DateTime? GetReleaseDate(Assembly? assembly = null)
+    {
+        object[]? attribute = (assembly ?? Assembly.GetEntryAssembly())?.GetCustomAttributes(typeof(ReleaseDateAttribute), false);
+        return attribute?.Length >= 1 ? ((ReleaseDateAttribute)attribute[0]).ReleaseDate : default;
+    }
+}
