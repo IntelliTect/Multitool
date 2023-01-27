a2f72ace5f58acc9ca63239c7249478cc8ce30a3
Benjamin Michaelis <benjamin@michaelis.net>
2023-01-26 17:39:20 -0800

    Replace AssemblyInfo with ReleaseDateAttribute (#58)
    Fixes #57

---
+++ IntelliTect.Multitool.Tests/ReleaseDateAttribute.Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace IntelliTect.Multitool.Tests;
+
+public class ReleaseDateAttributeTests
+{
+    [Fact]
+    public void ReleaseDateIsGettingInjected()
+    {
+        DateTime? releaseDate = ReleaseDateAttribute.GetReleaseDate(GetType().Assembly);
+        Assert.NotNull(releaseDate);
+        Assert.Equal(DateTimeKind.Utc, releaseDate?.Kind);
+        Assert.NotEqual(DateTime.MinValue, releaseDate);
+        Assert.NotEqual(DateTime.MaxValue, releaseDate);
+    }
+}
---
+++ IntelliTect.Multitool/ReleaseDateAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.Reflection;
+
+namespace IntelliTect.Multitool;
+
+/// <summary>
+/// Information about the executing assembly.
+/// </summary>
+[AttributeUsage(AttributeTargets.Assembly)]
+public class ReleaseDateAttribute : Attribute
+{
+    /// <summary> 
+    /// Constructor called from csproj file
+    /// </summary>
+    /// <param name="utcDateString">A utc O (round-trip date/time) format string</param>
+    public ReleaseDateAttribute(string utcDateString)
+    {
+        ReleaseDate = DateTime.ParseExact(utcDateString, "O", CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal);
+    }
+    /// <summary>
+    /// The date the assembly was built
+    /// </summary>
+    public DateTime ReleaseDate { get; }
+    /// <summary>
+    /// Method to obtain the release date from the assembly attributes
+    /// </summary>
+    /// <param name="assembly">An assembly instance</param>
+    /// <returns>The date time from compilation time</returns>
+    public static DateTime? GetReleaseDate(Assembly? assembly = null)
+    {
+        object[]? attribute = (assembly ?? Assembly.GetEntryAssembly())?.GetCustomAttributes(typeof(ReleaseDateAttribute), false);
+        return attribute?.Length >= 1 ? ((ReleaseDateAttribute)attribute[0]).ReleaseDate : null;
+    }
+}
