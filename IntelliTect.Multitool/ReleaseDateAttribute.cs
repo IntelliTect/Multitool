459b7a7d82151e050efbe2208180b877b4c9561c
Benjamin Michaelis <benjamin@michaelis.net>
2023-02-10 10:47:48 -0800

    Update comments

--- IntelliTect.Multitool/ReleaseDateAttribute.cs
+++ IntelliTect.Multitool/ReleaseDateAttribute.cs
@@ -4,31 +4,34 @@ using System.Reflection;
 namespace IntelliTect.Multitool;
 
 /// <summary>
-/// Information about the executing assembly.
+/// The release date assembly attribute.
 /// </summary>
 [AttributeUsage(AttributeTargets.Assembly)]
 public class ReleaseDateAttribute : Attribute
 {
     /// <summary> 
-    /// Constructor called from .targets file
+    /// Constructor that takes in a DateTime string
     /// </summary>
-    /// <param name="utcDateString">A utc O (round-trip date/time) format string</param>
+    /// <param name="utcDateString">A DateTime 'O' (round-trip date/time) format string</param>
     public ReleaseDateAttribute(string utcDateString)
     {
         ReleaseDate = DateTime.ParseExact(utcDateString, "O", CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind);
     }
+
     /// <summary>
     /// The date the assembly was built
     /// </summary>
     public DateTime ReleaseDate { get; }
+
     /// <summary>
-    /// Method to obtain the release date from the assembly attributes
+    /// Method to obtain the release date from the assembly attribute
     /// </summary>
     /// <param name="assembly">An assembly instance</param>
-    /// <returns>The date time from compilation time</returns>
+    /// <returns>The date time from the assembly attribute</returns>
     public static DateTime? GetReleaseDate(Assembly? assembly = null)
     {
         object[]? attribute = (assembly ?? Assembly.GetEntryAssembly())?.GetCustomAttributes(typeof(ReleaseDateAttribute), false);
         return attribute?.Length >= 1 ? ((ReleaseDateAttribute)attribute[0]).ReleaseDate : null;
     }
+    
 }
