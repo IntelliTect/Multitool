c07b642ab22960ef016969c189dd7b6b814e810f
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2023-11-04 22:02:55 +0100

    Write test to read from file

--- IntelliTect.Multitool.Tests/RepositoryPaths.Tests.cs
+++ IntelliTect.Multitool.Tests/RepositoryPaths.Tests.cs
@@ -10,4 +10,20 @@ public class RepositoryPathsTests
         // Makes the assumption that the repository directory for this solution is named the same as the solution
         Assert.EndsWith(nameof(Multitool), RepositoryPaths.GetDefaultRepoRoot());
     }
+
+    [Fact]
+    public void VariablesAreGettingSet()
+    {
+        string[] value = File.ReadAllLines(Path.Combine(Path.GetTempPath(), "IntelliTect.MultiTool.BuildVariables.tmp"));
+        // Build a dictionary of the values and their associated keys from the file
+        // Example: BuildingForLiveUnitTesting::null is a value of null for the key BuildingForLiveUnitTesting
+        // If value is null or empty, set value to null in dictionary
+        Dictionary<string, string?> dictionary = value
+            .Select(line => line.Split("::"))
+            .ToDictionary(
+                           split => split[0],
+                                          split => !string.IsNullOrEmpty(split[1]) ? split[1] : null);
+        Assert.Equal(2, dictionary.Count);
+        Assert.Null(dictionary["BuildingForLiveUnitTesting"]);
+    }
 }
