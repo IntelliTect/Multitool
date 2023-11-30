00996bddbeb7bca4d00f3f3ee34bfec3392ee6e1
Benjamin Michaelis <github@relay.benjamin.michaelis.net>
2023-11-29 20:56:01 -0800

    feat: Add System.Linq WhereNotNull() extension (#100)
    This solves the case of `foreach (string folder in folders.Where(x =>
    !string.IsNullOrWhiteSpace(x)))` not working by itself and needing to
    cast it or similar

---
+++ IntelliTect.Multitool/Extensions/SystemLinqExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace IntelliTect.Multitool.Extensions;
+
+/// <summary>
+/// Various Linq extensions
+/// </summary>
+public static class SystemLinqExtensions
+{
+    /// <summary>
+    /// Filters a sequence of values to only those that are not null.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements of source.</typeparam>
+    /// <param name="source">A <see cref="IEnumerable{T}"/> to filter.</param>
+    /// <returns>An <see cref="IEnumerable{T}"/> that contains elements from the input that are not null.</returns>
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source) where T : class?
+    {
+        return (IEnumerable<T>)source.Where(item => item is not null);
+    }
+}
