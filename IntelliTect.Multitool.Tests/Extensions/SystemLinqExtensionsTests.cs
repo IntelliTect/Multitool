9d8b96b825c686eea8fcb07db1466952cccdc53d
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2023-11-30 08:49:54 -0800

    test: WhereNotNull

---
+++ IntelliTect.Multitool.Tests/Extensions/SystemLinqExtensionsTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace IntelliTect.Multitool.Extensions.Tests;
+
+public class SystemLinqExtensionsTests
+{
+    private static readonly List<string?> stringsWithSomeNullValues = ["Hello", null, "World", null, "!"];
+
+    [Fact]
+    public void WhereNotNull_ReturnsOnlyNonNullValues()
+    {
+        IEnumerable<string> nonNullableStringType = stringsWithSomeNullValues.WhereNotNull();
+        Assert.All(stringsWithSomeNullValues.WhereNotNull(), s => Assert.NotNull(s));
+        Assert.Equal(new List<string> { "Hello", "World", "!" }, nonNullableStringType);
+    }
+}
